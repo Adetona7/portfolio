52f37b69ce11795dde7122c4c77373139b050bb4
Adetona7 <adetonabukum@gmail.com>
2026-04-28 22:33:46 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./app.css"; 
+import NavigationMenu from './components/Navbar';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <NavigationMenu />
+    </div>  
+  )
+}
+
+export default App;
